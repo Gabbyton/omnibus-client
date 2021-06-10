5c861caf048e0441d6c4af64c2b926be4ad232b0
startec-official <startec.innovations@gmail.com>
2021-06-10 08:11:39 -0400

    finished segment service

---
+++ src/app/utils/data/services/segment.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+import { catchError, concatMap, tap } from "rxjs/operators";
+import { TranslocService } from "../../transloc.service";
+import { Route } from "../models/route.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SegmentService {
+    private segments: Map<number, any[]> = new Map();
+
+    constructor(private transloc: TranslocService) { }
+
+    prefetch(allRoutes: Route[]): Observable<boolean> {
+        return this.transloc.getSegmentsForAllRoutes(allRoutes).pipe(
+            tap(data => { this.segments = data }),
+            concatMap(_ => of(true)),
+            catchError(_ => of(false)),
+        );
+    }
+
+    getSegment(routeID: number): any[] {
+        return this.segments.get(routeID);
+    }
+}
