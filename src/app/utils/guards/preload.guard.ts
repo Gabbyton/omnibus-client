dbe3542287b7d694e33b95852c6fea7844299ad4
startec-official <startec.innovations@gmail.com>
2021-06-10 09:08:58 -0400

    added preload guard

---
+++ src/app/utils/guards/preload.guard.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Resolve } from '@angular/router';
+import { UiService } from '../ui.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PreloadGuard implements Resolve<any> {
+
+  constructor(private uiService: UiService) { }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.uiService.prefetchMapData();
+  }
+}
