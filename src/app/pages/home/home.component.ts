0b05188830a30288428af39f0f60377532fd2802
startec-official <startec.innovations@gmail.com>
2021-06-11 10:28:45 -0400

    added button overlay

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
-import { AfterViewChecked, Component, DoCheck, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav/drawer';
 import { DrawerService } from 'src/app/utils/ui-services/drawer.service';
 import { SplashService } from 'src/app/utils/ui-services/splash.service';
-import { UiService } from 'src/app/utils/ui-services/ui.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
@@ -12,11 +13,27 @@ import { UiService } from 'src/app/utils/ui-services/ui.service';
 export class HomeComponent implements OnInit {
   @ViewChild('drawer') drawer: MatDrawer;
   currentRouteName: string;
+  toggleButtons = [
+    { icon: 'favorite' },
+    { icon: 'own-toggle' },
+    { icon: 'bus-toggle' },
+    { icon: 'group-toggle' },
+    { icon: 'pool-toggle' },
+  ]
 
   constructor(
     private drawerService: DrawerService,
     private splashService: SplashService,
-  ) { }
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer
+  ) {
+    iconRegistry.addSvgIcon('logo', sanitizer.bypassSecurityTrustResourceUrl('../assets/icons/menu-icons/logo-white.svg'));
+    iconRegistry.addSvgIcon('account', sanitizer.bypassSecurityTrustResourceUrl('../assets/icons/menu-icons/account-white.svg'));
+    this.toggleButtons.forEach(element => {
+      console.log(element.icon);
+      iconRegistry.addSvgIcon(element.icon, sanitizer.bypassSecurityTrustResourceUrl(`../assets/icons/toggle-buttons/${element.icon}.svg`));
+    });
+  }
 
   ngOnInit(): void {
     this.splashService.hide();
