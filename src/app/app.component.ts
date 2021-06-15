a2c27af5427c1927e36ac7e1c5051a22503be0a5
startec-official <startec.innovations@gmail.com>
2021-06-15 09:00:04 -0400

    added marker animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as SlidingMarker from "node_modules/marker-animate-unobtrusive/dist/SlidingMarker.min.js";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'transloc-app-wrapper';
+
+  ngOnInit(): void {
+    // TODO: optimize import
+    SlidingMarker.initializeGlobally();
+  }
 }
