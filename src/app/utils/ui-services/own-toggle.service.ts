d22644f8c882ba2567bb0a16f432677f97b878ed
startec-official <startec.innovations@gmail.com>
2021-06-16 11:04:26 -0400

    added socket related functionality in own-toggle

--- src/app/utils/ui-services/own-toggle.service.ts
+++ src/app/utils/ui-services/own-toggle.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { first } from 'rxjs/operators';
+import { RouteService } from '../data/model-services/route.service';
+import { SocketService } from '../data/web-services/socket.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OwnToggleService {
+  constructor(
+    private socketService: SocketService,
+    private routeService: RouteService,
+  ) { }
 
-  constructor() { }
+  // TODO: get current stop
+  // TODO: create a stop service for getting the nearest stpo
+  private getStopId() {
+    // TODO: replace with stop id
+    return 0;
+  }
+
+  toggleShowLocation(currentlyShowingLocation: boolean) {
+    if (currentlyShowingLocation == true) {
+      // turn off show location
+      this.socketService.disconnect();
+    } else {
+      this.socketService.routeSubscribe(this.routeService.currentRouteIDValue, this.getStopId());
+      this.socketService.onRouteJoined().pipe(
+        first(),
+      ).subscribe(joinData => {
+        // additional on route join operations here
+        // TODO: remove
+        console.log('room successfully joined!');
+        console.log(joinData);
+      });
+    }
+  }
 }
